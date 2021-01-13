4d2a52da0558a928fc2ccf3cb9119f6a07c3b68f
Radek25 <66422260+Radek25@users.noreply.github.com>
2021-01-13 16:08:54 +0100

    Add LocalStorage function

--- Cw5_Pogoda/script.js
+++ Cw5_Pogoda/script.js
@@ -1,9 +1,13 @@
 const key = '37300b07121f5e169e690b82550938f4';
+
+const LSkey = 'Miasta';
+let CityArray = [];
+CityArray.push(...GetFromLocalStorage());
+
 const content = {city: null, country: null, temp: null, cloud: null, wind: null,};
 
 const contentbox = document.querySelector('#content');
 const searchIcon = document.querySelector('.fas');
-let weatherCounter = 0;
 
 //Pobranie miasta z input text
 function LocationFromUserInput(){
@@ -13,10 +17,19 @@ function LocationFromUserInput(){
 //Przycisk wyszukaj
 searchIcon.addEventListener('click', TakeData);
 
-//Pobranie informacji pogodowej dla wskazanego miasta
 function TakeData(){
-    let city = LocationFromUserInput();
-    let weather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pl&APPID=${key}`;
+    CityArray.push(LocationFromUserInput());
+    SetToLocalStorage();
+    contentbox.innerHTML ='';
+    CityArray.forEach(element => GetWeather(element));
+}
+
+CityArray.forEach(element => GetWeather(element));
+
+function GetWeather(element){
+
+    console.log(element);
+    let weather = `https://api.openweathermap.org/data/2.5/weather?q=${element}&lang=pl&APPID=${key}`;
 
     fetch(weather)
         .then(function(response){
@@ -34,51 +47,55 @@ function TakeData(){
             CreateDivWithInfo();
         });
 }
+
 function CreateDivWithInfo(){
-    if (weatherCounter < 4) {
-        const weatherbox = document.createElement('div');
-        weatherbox.classList.add('weatherbox');
-        contentbox.appendChild(weatherbox);
-        weatherCounter++;
+    const weatherbox = document.createElement('div');
+    weatherbox.classList.add('weatherbox');
+    contentbox.appendChild(weatherbox);
+
         
-        //WeatherBox Elements
-        const location = document.createElement('div');
-        location.classList.add('location');
-        weatherbox.appendChild(location);
-        location.innerHTML = content.city + ', ' + content.country;
+    //WeatherBox Elements
+    const location = document.createElement('div');
+    location.classList.add('location');
+    weatherbox.appendChild(location);
+    location.innerHTML = content.city + ', ' + content.country;
 
-        const temp = document.createElement('div');
-        temp.classList.add('temp');
-        weatherbox.appendChild(temp);
-        temp.innerHTML = content.temp + ' &deg' + 'C';
+    const temp = document.createElement('div');
+    temp.classList.add('temp');
+    weatherbox.appendChild(temp);
+    temp.innerHTML = content.temp + ' &deg' + 'C';
 
-        const icon = document.createElement('div');
-        icon.classList.add('icon');
-        weatherbox.appendChild(icon);
+    const icon = document.createElement('div');
+    icon.classList.add('icon');
+    weatherbox.appendChild(icon);
 
-        const info = document.createElement('div');
-        info.classList.add('info');
-        weatherbox.appendChild(info);
+    const info = document.createElement('div');
+    info.classList.add('info');
+    weatherbox.appendChild(info);
 
-        const cloud = document.createElement('div');
-        cloud.classList.add('cloud');
-        info.appendChild(cloud);
-        cloud.innerHTML = '<i class="fas fa-cloud"></i>' + content.cloud + '%';
+    const cloud = document.createElement('div');
+    cloud.classList.add('cloud');
+    info.appendChild(cloud);
+    cloud.innerHTML = '<i class="fas fa-cloud"></i>' + content.cloud + '%';
 
-        const wind = document.createElement('div');
-        wind.classList.add('wind');
-        info.appendChild(wind);
-        wind.innerHTML = '<i class="fas fa-wind"></i>' + content.wind + ' m/s';
+    const wind = document.createElement('div');
+    wind.classList.add('wind');
+    info.appendChild(wind);
+    wind.innerHTML = '<i class="fas fa-wind"></i>' + content.wind + ' m/s';
 
-        const date = document.createElement('div');
-        date.classList.add('date');
-        weatherbox.appendChild(date);
-    }
-    else if (weatherCounter == 4){
-        alert ('Zbyt wiele lokalizacji! Usuń nieużywane!');
-    }
+    const date = document.createElement('div');
+    date.classList.add('date');
+    weatherbox.appendChild(date);
 }
 
-
-
-
+//Funkcje Local Storage
+function SetToLocalStorage(){
+    localStorage.setItem(LSkey, JSON.stringify(CityArray));
+}
+function GetFromLocalStorage(){
+    const GetCity = localStorage.getItem(LSkey);
+    if (GetCity) {
+        return JSON.parse(GetCity);
+    }
+    return[];
+}
