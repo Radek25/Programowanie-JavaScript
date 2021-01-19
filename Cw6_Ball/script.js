46acffc8ae09c6fe5e98c038bf71d8f4f098feb8
Radek25 <66422260+Radek25@users.noreply.github.com>
2021-01-19 22:03:30 +0100

    Create first version of game

--- Cw6_Ball/script.js
+++ Cw6_Ball/script.js
@@ -1,40 +1,80 @@
 let playGround = document.querySelector('#playground');
+let RoundCounter = 1;
 
-let hole = document.createElement('div');
-hole.classList.add('hole');
-playGround.appendChild(hole);
+let HoleTop = 0;
+let HoleLeft = 0;
+let ball = document.querySelector('#ball');
+let IsGameEnded= false;
+let playGroundHeight = playGround.clientHeight;
+let playGroundWidth = playGround.clientWidth;
+
 
 function GetRandomFromToX(min, max) {
     min = Math.ceil(15);
-    max = Math.floor(90);
+    max = Math.floor(70);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function GetRandomFromToY(min, max) {
     min = Math.ceil(5);
-    max = Math.floor(85);
+    max = Math.floor(80);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let a = GetRandomFromToX();
-let b = GetRandomFromToY();
-hole.style.top = a +'vh';
-hole.style.left = b +'vw';
+function HoleGenerator() {
+    for (let index = 0; index < RoundCounter; index++) {
+        let a = GetRandomFromToX();
+        let b = GetRandomFromToY();
 
+        // const holes = document.querySelector('.hole');
+        // for (let i = 0; i < holes.length; i++) {
+        //     const top = holes[i].style
+        // }
+        
+        let hole = document.createElement('div');
+        hole.classList.add('hole');
+        playGround.appendChild(hole);
 
-//Akcelelator
-function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
-};
+        hole.style.top = a +'%';
+        hole.style.left = b +'%';
+        HoleTop = hole.offsetTop;
+        HoleLeft = hole.offsetLeft;
+    }
+}
+HoleGenerator();
 
-function onError() {
-    alert('onError!');
-};
+// window.setInterval(location.reload(true), 2000);
 
-var options = { frequency: 3000 };  // Update every 3 seconds
+// eslint-disable-next-line no-undef
+let acl = new Accelerometer({frequency: 60});
 
-let watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-console.log(watchID);
+acl.addEventListener('reading', () => {
+    // console.log('Acceleration along the X-axis ' + acl.x);
+    // console.log('Acceleration along the Y-axis ' + acl.y);
+    if (!IsGameEnded) {
+        const ballTop = parseInt(ball.style.top || 0);
+        const ballLeft = parseInt(ball.style.left || 0);
+        const top = ballTop + acl.y;
+        const left = ballLeft - acl.x;
+    
+        if(top > 0 && top < playGroundHeight){
+            ball.style.top = top + 'px';
+        }
+    
+        if (left > 0 && left < playGroundWidth) {
+            ball.style.left = left + 'px';
+        }
+    
+        const IsInHoleTop = ballTop >= HoleTop && ballTop + 20 <= HoleTop + 50;
+        const IsInHoleLeft = ballLeft >= HoleLeft && ballLeft + 20 <= HoleLeft + 50;
+        
+        if ( IsInHoleTop && IsInHoleLeft) {
+            IsGameEnded = true;
+        }
+    }
+    else{
+        //
+    }
+});
+
+acl.start();
